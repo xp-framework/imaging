db057b983c6912d28954683de2bc8ef58a380a58
Timm Friebe <thekid@thekid.de>
2024-12-22 18:38:57 +0100

    Correctly look up "LensModel" in EXIF

--- src/main/php/img/io/ImageMetaData.class.php
+++ src/main/php/img/io/ImageMetaData.class.php
@@ -161,10 +161,10 @@ class ImageMetaData {
 
       $data->withMake(null === ($l= self::lookup($raw, 'Make')) ? null : trim($l));
       $data->withModel(null === ($l= self::lookup($raw, 'Model')) ? null : trim($l));
-      $data->withLensModel(null === ($l= self::lookup($raw, 'LensModel')) ? null : trim($l));
       $data->withSoftware(null === ($l= self::lookup($raw, 'Software')) ? null : trim($l));
 
       $exif= $raw['Exif_IFD_Pointer']['data'];
+      $data->withLensModel(null === ($l= self::lookup($exif, 'LensModel')) ? null : trim($l));
 
       if ($sof= $this->segmentsOf(SOFNSegment::class)) {
         $data->withWidth($sof[0]->width());
